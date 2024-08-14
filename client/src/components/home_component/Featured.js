41651bc3d1faee0111770d6d7181fc0111fdd757
castro876 <jovmrt@gmail.com>
2024-08-14 12:46:42 -0500

    6th commit

--- client/src/components/home_component/Featured.js
+++ client/src/components/home_component/Featured.js
@@ -1,97 +1,205 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
-const ProductCard = ({ product }) => {
-  return (
-    <div className="d-inline-block" style={{ width: "48%", marginRight: "12px" }}>
-      <img
-        className="border"
-        src={`/images/${product.image}`}
-        alt={product.title}
-        style={{ width: "100%" }}
-      />
-      <p className="text-white py-2" style={{ backgroundColor: "black" }}>
-        {product.gender}
-      </p>
-      <div className="text-start">
-        <p>{product.title}</p>
-        <s className="text-danger">${product.discount}</s>
-        <span>${product.price}</span>
-        <br />
-        <span>{product.discount_details}</span>
-        <br />
-        <span>
-          <i className="fa fa-star" aria-hidden="true"></i>
-          <i className="fa fa-star" aria-hidden="true"></i>
-          <i className="fa fa-star" aria-hidden="true"></i>
-          <i className="fa fa-star" aria-hidden="true"></i>
-          <i className="fa fa-star" aria-hidden="true"></i>
-          <span>&nbsp;(5.5)</span>
-        </span>
-        <br />
-        <br />
-        <Link to={`/singleproduct/${product._id}`}>
-          <button className="btn btn-dark">Buy Now &rarr;</button>
-        </Link>
-      </div>
-    </div>
-  );
-};
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 const Featured = () => {
-  const [prodData, setProdData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          "https://reseller-shop-backend.onrender.com/all_product"
-        );
-        const data = await response.json();
-        setProdData(data);
-      } catch (error) {
-        console.error("Error fetching product data:", error);
-      }
-    };
+  const [prodData, setproData] = useState(null)
 
-    fetchData();
-  }, []);
+    useEffect(() => {
+         const func = async () => {
+                const response = await fetch('https://reseller-shop-backend.onrender.com/all_product')
+                const data = await response.json()
+                setproData(data)
+         }
+           
+         func()
+    },[])
 
-  return (
-    <>
-      <div className="container-fluid btn-cont">
-        <h2 style={{ marginTop: "20px", marginBottom: "20px" }}>
-          <span className="text-danger">Featured</span> Products{" "}
-          <span style={{ fontSize: "35px" }}>&rarr;</span>
-        </h2>
-        <div className="row border-0 mb-4 gx-1 gy-3 text-center">
-          {prodData.slice(0, 2).map((product, index) => (
-            <div className="col-sm" key={index}>
-              <ProductCard product={product} />
-            </div>
-          ))}
-        </div>
+    return ( 
+        <>
+  <div className="container-fluid btn-cont">
+  <h2 style={{"marginTop":"20px", "marginBottom":"20px"}}><span className="text-danger">Featured</span> Products <span style={{'fontSize':"35px"}}>&rarr;</span></h2>
+  <div className="row border border-0 mb-4 gx-1 gy-3 text-center">
+   
+  <div className="col-sm">
+      <div className="d-inline-block" style={{"width":"48%","marginRight":"12px"}}>
+        <img className="border" src={`/images/${prodData && prodData[0].image}`} alt="product" style={{"width":"100%"}}/>
+        <p className="text-white py-2" style={{"backgroundColor":"black"}}>{prodData && prodData[0].gender}</p>
+        <div className="text-start">
+        <p>{prodData && prodData[0].title}</p>
+        <s className="text-danger">${prodData && prodData[0].discount}</s>
+        <span>${prodData && prodData[0].price}</span><br />
+        <span>{prodData && prodData[0].discount_details}</span><br />
+        <span>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <span>&nbsp;(5.5)</span>
+        </span><br /><br />
+        <Link to={`/singleproduct/${prodData && prodData[0]._id}`}><button className="btn btn-dark">Buy Now &rarr;</button></Link>
       </div>
-
-      <div className="container-fluid btn-cont">
-        <h2 style={{ marginTop: "40px", marginBottom: "20px" }}>
-          <span className="text-danger">Latest</span> Products{" "}
-          <span style={{ fontSize: "35px" }}>&rarr;</span>
-        </h2>
-        <div className="row border-0 mb-4 gx-1 gy-3 text-center">
-          {prodData.slice(2, 4).map((product, index) => (
-            <div className="col-sm" key={index}>
-              <ProductCard product={product} />
-            </div>
-          ))}
-        </div>
       </div>
-
-      <div className="view-box">
-        <p>&larr; Load More &rarr;</p>
+    
+    <div className="d-inline-block" style={{"width":"48%"}}>
+    <img className="border" src="/images/home_pic2.png" alt="product" style={{"width":"100%"}}/>
+    <p className="text-white py-2" style={{"backgroundColor":"black"}}>Men</p>
+    <div className="text-start">
+        <p>Item-2</p>
+        <s className="text-danger">$150.00</s>
+        <span>$50.00</span><br />
+        <span>Price Reflects 40% Off</span><br />
+        <span>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <span>&nbsp;(5.5)</span>
+        </span><br /><br />
+        <button className="btn btn-dark">Buy Now &rarr;</button>
       </div>
-    </>
+    </div>
+    </div>
+
+    <div className="col-sm">
+      <div className="d-inline-block" style={{"width":"48%","marginRight":"12px"}}>
+        <img className="border" src="/images/home_pic2.png" alt="product" style={{"width":"100%"}}/>
+        <p className="text-white py-2" style={{"backgroundColor":"black"}}>Women</p>
+        <div className="text-start">
+        <p>Item-3</p>
+        <s className="text-danger">$150.00</s>
+        <span>$50.00</span><br />
+        <span>Price Reflects 40% Off</span><br />
+        <span>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <span>&nbsp;(5.5)</span>
+        </span><br /><br />
+        <button className="btn btn-dark">Buy Now &rarr;</button>
+      </div>
+      </div>
+    
+    <div className="d-inline-block" style={{"width":"48%"}}>
+    <img className="border" src="/images/home_pic2.png" alt="product" style={{"width":"100%"}}/>
+    <p className="text-white py-2" style={{"backgroundColor":"black"}}>Men</p>
+    <div className="text-start">
+        <p>Item-4</p>
+        <s className="text-danger">$150.00</s>
+        <span>$50.00</span><br />
+        <span>Price Reflects 40% Off</span><br />
+        <span>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <span>&nbsp;(5.5)</span>
+        </span><br /><br />
+        <button className="btn btn-dark">Buy Now &rarr;</button>
+      </div>
+    </div>
+    </div>
+  </div>
+    </div>
+
+    <div className="container-fluid btn-cont">
+    <h2 style={{"marginTop":"40px", "marginBottom":"20px"}}><span className="text-danger">Latest</span> Products <span style={{'fontSize':"35px"}}>&rarr;</span></h2>
+  <div className="row border border-0 mb-4 gx-1 gy-3 text-center">
+   
+  <div className="col-sm">
+      <div className="d-inline-block" style={{"width":"48%","marginRight":"12px"}}>
+        <img className="border" src="/images/home_pic2.png" alt="product" style={{"width":"100%"}}/>
+        <p className="text-white py-2" style={{"backgroundColor":"black"}}>Women</p>
+        <div className="text-start">
+        <p>Item-1</p>
+        <s className="text-danger">$150.00</s>
+        <span>$50.00</span><br />
+        <span>Price Reflects 40% Off</span><br />
+        <span>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <span>&nbsp;(5.5)</span>
+        </span><br /><br />
+        <button className="btn btn-dark">Buy Now &rarr;</button>
+      </div>
+      </div>
+    
+    <div className="d-inline-block" style={{"width":"48%"}}>
+    <img className="border" src="/images/home_pic2.png" alt="product" style={{"width":"100%"}}/>
+    <p className="text-white py-2" style={{"backgroundColor":"black"}}>Men</p>
+    <div className="text-start">
+        <p>Item-2</p>
+        <s className="text-danger">$150.00</s>
+        <span>$50.00</span><br />
+        <span>Price Reflects 40% Off</span><br />
+        <span>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <span>&nbsp;(5.5)</span>
+        </span><br /><br />
+        <button className="btn btn-dark">Buy Now &rarr;</button>
+      </div>
+    </div>
+    </div>
+
+    <div className="col-sm">
+      <div className="d-inline-block" style={{"width":"48%","marginRight":"12px"}}>
+        <img className="border" src="/images/home_pic2.png" alt="product" style={{"width":"100%"}}/>
+        <p className="text-white py-2" style={{"backgroundColor":"black"}}>Women</p>
+        <div className="text-start">
+        <p>Item-3</p>
+        <s className="text-danger">$150.00</s>
+        <span>$50.00</span><br />
+        <span>Price Reflects 40% Off</span><br />
+        <span>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <span>&nbsp;(5.5)</span>
+        </span><br /><br />
+        <button className="btn btn-dark">Buy Now &rarr;</button>
+      </div>
+      </div>
+    
+    <div className="d-inline-block" style={{"width":"48%"}}>
+    <img className="border" src="/images/home_pic2.png" alt="product" style={{"width":"100%"}}/>
+    <p className="text-white py-2" style={{"backgroundColor":"black"}}>Men</p>
+    <div className="text-start">
+        <p>Item-4</p>
+        <s className="text-danger">$150.00</s>
+        <span>$50.00</span><br />
+        <span>Price Reflects 40% Off</span><br />
+        <span>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <i class="fa fa-star" aria-hidden="true"></i>
+        <span>&nbsp;(5.5)</span>
+        </span><br /><br />
+        <button className="btn btn-dark">Buy Now &rarr;</button>
+      </div>
+    </div>
+    </div>
+  </div>
+    </div>   
+<br/>
+ <div className="view-box"><p>&larr; Load More &rarr;</p></div>
+   </>
   );
-};
-
+}
+ 
 export default Featured;
